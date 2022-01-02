819b68f17cd60b77201b382c4d306474106f9294
PredatoryBalloon <52945286+PredatoryBalloon@users.noreply.github.com>
2022-01-02 12:45:22 -0600

    Fixed creator name field issue.

---
+++ Distance.EditorAnnihilator/Harmony/Assembly-CSharp/LevelSettings/OpenLevelSelectMenu.cs
@@ -0,0 +1,33 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using HarmonyLib;
+using LevelEditorTools;
+using UnityEngine;
+using System.Reflection;
+
+namespace Mod.EditorAnnihilator.Harmony
+{
+    [HarmonyPatch(typeof(LevelEditor), "OpenLevelSelectMenu")]
+    internal static class LevelEditor__OpenLevelSelectMenu
+    {
+        //__instance is the class you are patching, so you can call functions on it.
+        //If patching a function with paramaters, you can just add those paramaters as paramaters inside Postfix.
+        //If the function you're patching has a return type, you can modify the result value with the parameter 'ref [type of return value] __result'
+        [HarmonyPrefix]
+        internal static void Prefix()
+        {
+            if (!Mod.CNF)
+            {
+                Mod.DevBuildForCreatorName = false;
+            }
+        }
+    }
+}
